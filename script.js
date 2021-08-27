e5e99ece06f00512f461c456a0c467a8551c1d92
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-08-26 21:02:42 -0500

    added some basic JS for questions.

---
+++ script.js
@@ -0,0 +1,25 @@
+var allQuestions = [{
+    question: "The condition is an if / else statement is enclosed within_."
+    choice: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"], 
+    correctAnswer: 
+},
+{
+    question: "Arrays in Javascript can be used to store_."
+    choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+    correctAnswer:
+},
+{  
+    question: "Commonly used data types do not include"
+    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+    correctAnswer:
+},
+{
+    question: "String values must be enclosed within __ when being assigned to variables."
+    choices: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
+    correctAnswer:
+},
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is: "
+    choices: ["Javascript", "Terminal/bash", "For Loops", "Console log"],
+    correctAnswer:
+}];
