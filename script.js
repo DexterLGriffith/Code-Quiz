6c60dac023b059036ae1cfc641e0d0f32f73b852
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-08-29 17:15:28 -0500

    changed up the main page layout, added title class, quiz is starting... still cant get questions to get called. possible script format changes.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const startButton = document.querySelector("#start-button");
 const allQuestionsContainer = document.querySelector("#allQuestions");
 const question = document.querySelector("#questionTitle");
+const answers = document.querySelector("#answersTitle");
 startButton.addEventListener("click", Start)
 
 // console.log("start")
@@ -9,7 +10,8 @@ function Start() {
     allQuestionsContainer.classList.remove("hide")
     allQuestions = {...question}
     score = 0 
-    setNextQuestion()
+    length =""
+    // setNextQuestion()
 
 
 
@@ -18,67 +20,89 @@ function Start() {
 
 
 }
-function setNextQuestion() {
-    showQuestion(allQuestions)
+var i = 0; 
+var lengthQ = allQuestions;
+var correctAnswer = true
 
+
+function showQuestion() {
+    var allQuestions = allQuestions[i];
+    questionTitle.innerText = allQuestions.question;
+
+    answersList.innerHTML = "";
+    for(key in allQuestions.answers){
+        var radioBtnName = "question" + i + "choice";
+        var choiceText = allQuestions.choices[key];
+        answersList.appendchild(createLi(radioBtnName,choiceText));
+
+    }
 }
-function showQuestion(question) {
-    question.innertext = question.question
-    question.answer.forEach(element => {
-        const button = document.createElement("type", "radio");
-        button.innerText = answer.text
-        button.classList.add("button")
-        
-    });
-}
+
+
+// function setNextQuestion() {
+//     showQuestion(allQuestions)
+// }
+// function showQuestion(question) {
+//     question.innertext = question.question
+//     // question.answers.for(answer => {
+//     //     const button = document.createElement("button")
+//     //     button.innerText = answer.text
+//     //     button.classList.add("button")
+//     //     if (answer.correct) {
+//     //         button.dataset.correct = answer.correct
+//     //     }
+//     //     button.addEventListener('click',userChoice)
+//     //     answers.appendChild(button)
+//     // });
+// }
 
 
 
 // format answer: {text: "___" correct: "___"}
 var allQuestions = [
     {
-    question: "The condition is an if / else statement is enclosed within_.",
+    question: "The condition in an if / else statement is enclosed within_.",
     answers: [
-        { text: "Quotes"},
-        { text: "Curly Brackets"},
-        { text: "Parentheses"},
-        { text: "Square Brackets"}
+        { text: "Quotes", correct: "false"},
+        { text: "Curly Brackets", correct: "true"},
+        { text: "Parentheses", correct: "false"},
+        { text: "Square Brackets", correct: "false"}
     ]
     },
     {
     question: "Arrays in Javascript can be used to store_.",
     answers: [
-        { text: "Numbers and Strings"},
-        { text: "Other Arrays"},
-        { text: "Booleans"},
-        { text: "All of the Above"}
+        { text: "Numbers and Strings", correct: "false"},
+        { text: "Other Arrays", correct: "false"},
+        { text: "Booleans", correct: "false"},
+        { text: "All of the Above", correct: "true"}
     ]
     },
     {  
     question: "Commonly used data types do not include",
     answers: [
-        { text: "Strings"},
-        { text: "Booleans"},
-        { text: "Alerts"},
-        { text: "Numbers"}
+        { text: "Strings", correct:"false"},
+        { text: "Booleans", correct:"false"},
+        { text: "Alerts", correct:"true"},
+        { text: "Numbers", correct:"false"}
     ]
     },
     {
     question: "String values must be enclosed within __ when being assigned to variables.",
     answers: [
-        { text: "Commas"},
-        { text: "Curly Brackets"},
-        { text: "Quotes"},
-        { text: "Parentheses"}
+        { text: "Commas", correct:"true"},
+        { text: "Curly Brackets", correct:"false"},
+        { text: "Quotes", correct:"false"},
+        { text: "Parentheses", correct:"false"}
     ]
     },
     {
     question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
     answers: [
-        { text: "Javascript"},
-        { text: "Terminal/bash"},
-        { text: "For Loops"},
-        { text: "Console log"}
+        { text: "Javascript", correct:"true"},
+        { text: "Terminal/bash", correct:"false"},
+        { text: "For Loops", correct:"false"},
+        { text: "Console log", correct:"false"}
     ]
     }
 ];
